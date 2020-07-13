98972332764de76fe36f12f5acb9970db1ebb2ad
Jonathan Robinson <jdswebservice@gmail.com>
2020-07-13 01:09:06 -0400

    Changed the route file

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@index')->name('index');
+// Normal Route
+// Route::get('/', 'PagesController@index')->name('index');
+// Coming Soon Route
+Route::get('/', 'PagesController@comingsoon')->name('index');
 
 Route::get('/comingsoon', 'PagesController@comingsoon')->name('comingsoon');
 
