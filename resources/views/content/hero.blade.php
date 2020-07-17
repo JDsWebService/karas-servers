715f3150ca3f7badaa0a82da2087a5fe40fb1e3f
Jonathan Robinson <jdswebservice@gmail.com>
2020-07-16 21:15:30 -0400

    Updated Hero

--- resources/views/content/hero.blade.php
+++ resources/views/content/hero.blade.php
@@ -7,15 +7,13 @@
             <div class="col-lg-6">
                 <div class="block">
                     <h2>Kara's Ark Servers</h2>
-                    <p>
-                        Kara's Worlds ARK servers started very humbly in September 2015. I hosted a single small ARK server on an old HP Laptop. I actually still own that original laptop, these days it does a better job as a bookend than ARK server. It started with just me and a friend. From that point I built a small custom PC in November 2015. I mentioned ARK to a few friends and from there was the start of Kara's ARK Servers.
-                    </p>
-                    <p>
-                        The first time that I had a large influx of players was the launch of the Scorched Earth DLC in September 2016. Due to some launch issues most cloud providers were unable to get private servers working for the first day or two. (linux issues). My small windows 10 pro PC was not up to the task of trying to handle dozens of players at the same time. We were instantly hit with 50 concurrent players and at about 55 ish people started to get disconnected. From this point it was sink or swim with hosting my own server.
-                    </p>
-                    <p>
-                        With the help of donations from a small core group of players I bought a used HP Z420 (Xeon E5-2670) to use a server in December 2016. I still have this server today.
-                    </p>
+                    <p>Kara's Worlds ARK servers started very humbly in September 2015. I hosted a single small ARK server on an old HP Laptop. I actually still own that original laptop, these days it does a better job as a bookend than ark server. It started with just me and a friend. From that point I built a small custom PC in November 2015. I mentioned ARK to a few friends and from there was the start of Kara's ARK Servers.</p>
+
+                    <p>The first time that I had a large influx of players was the launch of the Scorched Earth DLC in September 2016. My small windows 10 pro PC was not up to the task of trying to handle dozens of players at the same time. We were instantly hit with 50 concurrent players and at about 55 ish people started to get disconnected. From this point it was sink or swim with hosting my own server.</p>
+
+                    <p>With the help of donations from a small core group of players I bought a used HP Z420 (Xeon E5-2670) to use a server in December 2016. Since then I have hosted ARK servers nearly continuously, among other game servers over time including Conan Exiles, Minecraft, 7d2d, and some other game servers.</p>
+
+                    <p>Today I have something of my own small server farm with multiple servers and PCs.</p>
                     <ul class="list-inline wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".7s">
                         <li class="list-inline-item">
                             <a href="#" target="_blank" class="btn btn-main">Join Our Discord</a>       
