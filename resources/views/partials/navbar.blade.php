61688c33c3e6a421a0af65ac7555bad2bd00c1f5
Jonathan Robinson <jdswebservice@gmail.com>
2020-08-01 23:25:00 -0400

    Navbar Style Changes for Users with No Avatar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -56,7 +56,10 @@
                             @auth
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <img src="{{ Auth::user()->avatar }}" alt="User's Avatar" class="navbar-avatar rounded-circle"> {{ Auth::user()->fullusername }}
+                                        @if(Auth::user()->avatar == NULL)
+                                            <img src="{{ Auth::user()->avatar }}" alt="User's Avatar" class="navbar-avatar rounded-circle"> 
+                                        @endif
+                                        {{ Auth::user()->fullusername }}
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @staff
