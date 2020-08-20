2ceddc6db31737c5b64cf1fddd9729cb63808eab
Jonathan Robinson <jdswebservice@gmail.com>
2020-08-19 23:02:26 -0400

    Profile Progress Bar Hotfix

--- resources/views/components/user-profile-progress.blade.php
+++ resources/views/components/user-profile-progress.blade.php
@@ -14,7 +14,12 @@
         <p>We want to get to know you a little better! Fill out your profile information below, once you click submit you'll see your profile card on the left update accordingly.</p>
         <p>
             <div class="progress">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-valuenow="{{ $progress->percentage }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress->percentage }}%">{{ $progress->percentage }}%</div>
+                @if($progress->percentage == '0')
+                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">0%</div>
+                @else
+                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-valuenow="{{ $progress->percentage }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress->percentage }}%">{{ $progress->percentage }}%</div>
+                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+                @endif
             </div>
         </p>
     </div>
